79501195aea1675fadd37b5fccf5ed3dafd638b8
Renzolxrd <renzo.davila27@gmail.com>
2025-04-30 16:29:39 -0300

    arreglo informe TP2

--- TP4-REDES/actividad1.py
+++ TP4-REDES/actividad1.py
@@ -2,16 +2,14 @@ import socket
 import threading
 
 def server():
-    socket_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket_server.bind("10.65.4.135", 60000)
+    socket_server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    socket_server.bind(("10.65.4.135", 60000))
     socket_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
-    socket_server.listen(5)
     print("Escuchando")
-    mensaje = socket_server.accept()
-    mensajeRecibido = mensaje[0].recv(100).decode("utf-8")
-    mensajeRecibido.split(":")
-    direccionMensaje = mensaje[1]
-    if mensajeRecibido[1] == "exit":
+    mensaje = socket_server.recvfrom(1000)
+    mensajeRecibido = mensaje[0].split(":")
+    direccion = mensaje[1][0]
+    if mensajeRecibido[0] == "exit":
         print(f"El usuario {mensajeRecibido[0]} ({direccionMensaje}) ha abandonado la conversacion")
     elif mensajeRecibido[1] == "nuevo":
         print(f"El usuario {mensajeRecibido[0]} se ha unido a la conversación")
@@ -19,10 +17,9 @@ def server():
         print(f"{mensajeRecibido[0]} ({direccionMensaje}) dice: {mensajeRecibido[1]}")
     
 def client():
-    socket_id = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_id = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print("Ingrese su nombre de usuario:")
     usuario = input()
-    socket_id.connect("10.65.4.255", 60000)
     socket_id.send((f"{usuario}:nuevo").encode())
     print("Mensaje a enviar: ")
     bufferSalida = input()
